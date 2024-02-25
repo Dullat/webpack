ec8e042f7ec5c1b1f21ec370d4984b0832911cd1
dullat <dullat@users.noreply.github.com>
2024-02-25 20:36:00 +0530

    update

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
         ]
     },
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
     plugins: [new htmlLoad()],
