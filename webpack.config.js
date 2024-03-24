3f971d2bf71fe3053347db670310cf65fe6b19ca
DuLLatJatt <142017979+Dullat@users.noreply.github.com>
2024-03-24 23:46:54 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,16 @@ module.exports = {
             {
                 test: /\.(jpg)$/i,
                 type: 'asset/resource'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    }
+                }
             }
         ]
     },
